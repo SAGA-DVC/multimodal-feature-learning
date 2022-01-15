6a099de75fca6224316d4c16c60cfe49d9eb987e
Akshat Shah <akshatshah_21@yahoo.in>
2022-01-15 14:46:41 +0530

    anet download script

--- data/anet/download.py
+++ data/anet/download.py
@@ -1,8 +1,4 @@
-'''
-Taken from https://github.com/skasai5296/actnetchallenge/blob/master/dataset/preparedataset.py
-'''
-
-import os
+import sys, os
 import json
 import time
 import argparse
@@ -14,27 +10,28 @@ from pytube import YouTube
 
 
 def urlgenerator(obj):
-    for key, value in obj.items():
-            yield key, f'https://www.youtube.com/watch?v={key[2:]}', value
+    for i in obj:
+        for key, value in obj.items():
+            if (key == 'database'):
+                for key2, value2 in value.items():
+                    yield key2, value2['url']
 
 def downloader(download_path, urlgen):
     cnt = 0
     failcnt = 0
     succeedcnt = 0
-    succeeded = {}
-    failed_ids = []
+    succeeded = []
     before = time.time()
-    for id, url, annotation in urlgen:
+    for id, url in urlgen:
         try:
             yt = YouTube(url)
             st = yt.streams.filter(file_extension='mp4').first()
-            st.download(output_path=download_path, filename=f"{id}.mp4")
+            st.download(output_path=download_path, filename=str("v_" + id + '.mp4'))
             succeedcnt += 1
-            succeeded[id] = annotation
+            succeeded.append(id)
             print('downloaded v_{}.mp4, total = {}'.format(id, succeedcnt), flush=True)
         except:
             failcnt += 1
-            failed_ids.append(id)
             print('could not download {}.mp4, failcount = {}'.format(id, failcnt), flush=True)
             continue
         if succeedcnt % 100 == 99:
@@ -42,14 +39,9 @@ def downloader(download_path, urlgen):
             print('{} videos done, {}s per loop'.format(succeedcnt+1, elapsed/5), flush=True)
             before = time.time()
         cnt += 1
-    
-    with open("available.json", "w") as f:
-        json.dump(succeeded, f)
-    with open("unavailable.json", "w") as f:
-        json.dump(failed_ids, f)
-
     print('complete, {}/{} videos in total'.format(succeedcnt, cnt), flush=True)
-
+    with open('succeeded.json', 'w') as file:
+        json.dump(succeeded, file)
 
 def main(args):
     if args.download_path is None:
@@ -61,7 +53,7 @@ def main(args):
         os.mkdir(path)
     with open(os.path.join(args.dset_root, args.json_path), 'r') as f:
         obj = json.load(f)
-        downloader(download_path, urlgenerator(obj))
+    downloader(download_path, urlgenerator(obj))
 
 
 if __name__ == '__main__':
