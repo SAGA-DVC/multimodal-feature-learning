c49e471cd6a084a488ab0112b91ea04348419872
arnavsshah <shah.arnav@yahoo.com>
2022-01-25 14:53:16 +0530

    added functionality to initialise or load weights into the bi-modal encoder

---
+++ models/bimodal_encoder.py
@@ -0,0 +1,147 @@
+""" Bimodal encoder for fusion of audio and video features """
+
+import torch
+import torch.nn as nn
+from torch.nn.init import trunc_normal_, zeros_, ones_
+
+from modules import BiModalEncoderBlock
+from load_weights import init_encoder_block_weights, load_bimodal_encoder_weights, load_classification_weights
+
+
+class BiModalEncoder(nn.Module):
+    def __init__(self, d_model=768, depth=12, num_heads=12, mlp_ratio=4., qkv_bias=True, 
+                attention_dropout=0., projection_dropout=0., dropout_1=0., dropout_2=0., 
+                classification_head=False, num_classes=None, return_preclassifier=False, return_prelogits=False, 
+                weight_init=False, weight_load=False, model_official=None):
+        
+        """
+        The Bi-modal Encoder which consists of cross attention modules between video and audio features.
+  
+        Parameters:
+            `d_model` (int): Dimension of the tensors used to compute attention
+            `depth` (int): number of encoder blocks. 
+            `num_heads` (int): Number of attention heads.
+            `mlp_ratio` (int): Used to determine the hidden layer dimension of the MLP. (default 4)
+            `qkv_bias` (boolean): Determines whether to use bias as part of the query/key/value linear layers in the attention block (default True)
+            `positional_embedding_dropout` (float): dropout probability for the positional embeddings (default 0.0)
+            `attention_dropout` (float): Dropout probability for the layer after the multi-head attention mechanism (default 0.0)
+            `dropout_1` (float): dropout probability for the MLP block (default 0.0)
+            `dropout_2` (float): dropout probability for the MLP block (default 0.0)
+            `classification_head` (boolean): If True, a classification head (fully connected layer) is added on top of the model (default False)
+            `num_classes` (int): number of classes for the prediction task (default None)
+            `return_preclassifier` (boolean): If True, return the representation after the transformer encoder. Useful if using this as the backbone stem as part of a bigger architecture (default False)
+            `return_prelogits` (boolean): If True, return the final representation of the network before the classification head. Useful when using features for a downstream task (default False)
+            `weight_init` (boolean): If True, initialises the weights of the model (default True)
+            `weight_load` (boolean): If True, loads the weights of the specified pre-trained model after initialisation (default False)
+            `model_official`: This model's weights are used by ViViT
+        """
+
+        super(BiModalEncoder, self).__init__()
+
+        self.d_model = d_model
+        self.num_classes = num_classes
+        self.depth = depth
+
+        self.classification_head = classification_head
+        self.return_prelogits = return_prelogits
+        self.return_preclassifier = return_preclassifier
+        
+        self.bi_modal_encoder = nn.Sequential(
+            *[
+                BiModalEncoderBlock(d_model=d_model,
+                            num_heads=num_heads,
+                            mlp_ratio=mlp_ratio,
+                            qkv_bias=qkv_bias,
+                            dropout_1=dropout_1,
+                            dropout_2=dropout_2,
+                            attention_dropout=attention_dropout,
+                            projection_dropout=projection_dropout
+                        )
+                for _ in range(depth)
+                ]
+            )
+
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+
+        self.head = nn.Linear(d_model, num_classes) if classification_head else nn.Identity() 
+
+        if weight_load and model_official is not None:
+            self.load_weights(model_official)
+
+        else:
+            self.init_weights()
+
+
+    def forward(self, vid, aud):
+        
+        """
+        Performs a forward pass on the Bi-modal encoder.
+  
+        Parameters:
+            vid (tensor): Tensor of dimension (batch_size, num_frames, d_model) representing video features
+            aud (tensor): Tensor of dimension (batch_size, num_tokens, d_model) representing audio features
+        
+        Returns:
+            x (tensor): if return_preclassifier is True, 2 Tensors of dimension 
+                            (batch_size, num_frames, d_model) for video features AND
+                            (batch_size, num_tokens, d_model) for audio features 
+                            
+                        if return_prelogits is True, Tensor of dimension (batch_size, 1, d_model) representing a
+                            fusion of video and audio features 
+
+                        else Tensor of dimension (batch_size, num_classes)
+        """
+
+        vid, aud = self.bi_modal_encoder(vid, aud) # (batch_size, num_frames, d_model), (batch_size, num_tokens, d_model)
+
+        if self.return_preclassifier :
+            return vid, aud
+
+        # some processing
+        x = vid 
+
+        if self.return_prelogits:
+            return x # (batch_size, 1, d_model)
+        
+        x = self.layer_norm(x)
+
+        x = self.head(x) # (batch_size, num_classes)
+        
+        return x
+
+
+    def init_weights(self):
+
+        """
+        Initialises the weights and biases of the all four ViViT models namely 
+        spatio temporal attention, factorised encoder, factorised self attention, factorised dot product attention.
+
+        These parameters include token embeddings, positional embeddings, [class] tokens (if any) and 
+        encoder blocks (consisting of linear layers and normalisation layers).
+        """
+
+        self.bi_modal_encoder.apply(init_encoder_block_weights)        
+
+        if self.classification_head:
+            ones_(self.layer_norm.weight)
+            zeros_(self.layer_norm.bias)
+
+            trunc_normal_(self.head.weight, std=.02)
+            trunc_normal_(self.head.bias, std=.02)
+
+
+
+    def load_weights(self, model_official):
+
+        """
+        Loads the weights and biases from the pre-trained model to the current model.
+
+        Parameters:
+            `model_custom`: The current ViViT model
+            `model_official`: The model which would be used to load the pre-trained weights
+        """
+
+        load_bimodal_encoder_weights(self, model_official)
+
+        if self.classification_head:
+            load_classification_weights(self, model_official)
---
+++ models/load_weights.py
@@ -0,0 +1,343 @@
+import torch
+import torch.nn as nn
+from torch.nn.init import trunc_normal_, zeros_, ones_
+import numpy as np
+
+def assert_tensors_equal(t1, t2):
+    a1, a2 = t1.detach().numpy(), t2.detach().numpy()
+    np.testing.assert_allclose(a1, a2)
+
+
+def init_encoder_block_weights(module):
+        """ 
+        Initialises the weights and biases of the Linear layers and Normalisation layers of the given module.
+
+        Parameters:
+            `module`: An entire model or specific part of it consisting of nn.Linear and nn.LayerNorm layers     
+        """
+
+        if isinstance(module, nn.Linear):
+            trunc_normal_(module.weight, std=.02)
+            zeros_(module.bias)
+        elif isinstance(module, nn.LayerNorm):
+            ones_(module.weight)
+            zeros_(module.bias)
+
+
+def load_token_embeddings(model_custom, model_official):
+
+    """
+    Loads the weights and biases for the token embeddings from the pre-trained model to the current model.
+
+    Parameters:
+        `model_custom`: The current ViViT model
+        `model_official`: The model which would be used to load the pre-trained weights
+        `tokenization_method` (string): One of 'filter inflation' or 'central frame'. Used for loading pre-trained weights for tubelet embedding
+    """
+    temporal_patch_size = model_custom.token_embeddings_layer.temporal_patch_size
+    
+    # (batch_size, num_channels, num_frames, width, height) -> model_custom token embeddings layer
+    # (batch_size, num_channels, width, height) -> model_official token embeddings layer
+
+    if temporal_patch_size > 1 and model_custom.tokenization_method == 'filter inflation':
+        model_custom.token_embeddings_layer.project_to_patch_embeddings \
+                    .weight.data[:] = torch.unsqueeze(model_official.patch_embed.proj.weight.data, 2) \
+                                    .repeat(1, 1, temporal_patch_size, 1, 1) / temporal_patch_size
+    
+    elif temporal_patch_size > 1 and model_custom.tokenization_method == 'central frame':
+        model_custom.token_embeddings_layer.project_to_patch_embeddings \
+                    .weight.data[:] = 0
+        model_custom.token_embeddings_layer.project_to_patch_embeddings \
+                    .weight.data[:, :, temporal_patch_size//2] = model_official.patch_embed.proj.weight.data
+    
+    else :
+        model_custom.token_embeddings_layer.project_to_patch_embeddings \
+                .weight.data[:] = torch.unsqueeze(model_official.patch_embed.proj.weight.data, 2)
+    
+
+    model_custom.token_embeddings_layer.project_to_patch_embeddings \
+                .bias.data[:] = model_official.patch_embed.proj.bias.data
+
+
+
+def load_positional_embeddings(model_custom, model_official):
+
+    """
+    Loads the positional embeddings from the pre-trained model to the current model for a specific model_name and whether the model uses a distillation token.
+
+    Parameters:
+        `model_custom`: The current ViViT model
+        `model_official`: The model which would be used to load the pre-trained weights
+        `model_name` (string): One of 'spatio temporal attention', 'factorised encoder', 'factorised self attention' or 'factorised dot product attention'
+    """
+
+    num_frames = model_custom.num_frames
+
+    # (1, num_frames, num_tokens, d_model) -> model_custom positional embeddings
+    # (1, num_patches, d_model) -> model_official positional embeddings
+
+    if model_custom.model_name == 'spatio temporal attention':
+        model_custom.encoder.add_positional_embedding_to_cls.data[:] = torch.unsqueeze(model_official.pos_embed.data[:, 0], 1)
+        
+        model_custom.encoder.add_positional_embedding \
+                    .positional_embedding.data[:] = torch.unsqueeze(model_official.pos_embed.data[:, 1:], 1).repeat(1, num_frames, 1, 1)
+
+
+    elif model_custom.model_name == 'factorised encoder':
+        if model_custom.distilled:
+            # cls
+            model_custom.encoder.add_positional_embedding_spatial \
+                    .positional_embedding.data[:, :, 0] = torch.unsqueeze(model_official.pos_embed.data[:, 0], 1).repeat(1, num_frames, 1)
+            
+            # other tokens
+            model_custom.encoder.add_positional_embedding_spatial \
+                    .positional_embedding.data[:, :, 2:] = torch.unsqueeze(model_official.pos_embed.data[:, 1:], 1).repeat(1, num_frames, 1, 1)
+
+            # temporal
+            trunc_normal_(model_custom.encoder.add_positional_embedding_temporal.positional_embedding, std=.02)
+
+        else:
+            model_custom.encoder.add_positional_embedding_spatial \
+                    .positional_embedding.data[:] = torch.unsqueeze(model_official.pos_embed.data, 1).repeat(1, num_frames, 1, 1)
+
+            trunc_normal_(model_custom.encoder.add_positional_embedding_temporal.positional_embedding, std=.02)
+
+
+    # no cls token
+    elif model_custom.model_name == 'factorised self attention' or model_custom.model_name == 'factorised dot product attention':
+        model_custom.encoder.add_positional_embedding \
+                    .positional_embedding.data[:] = torch.unsqueeze(model_official.pos_embed.data[:, 1:], 1).repeat(1, num_frames, 1, 1)
+
+
+
+def load_cls_tokens(model_custom, model_official):
+    
+    """
+    Loads the [class] token from the pre-trained model to the current model
+
+    Parameters:
+        `model_custom`: The current ViViT model
+        `model_official`: The model which would be used to load the pre-trained weights
+        `model_name` (string): One of 'spatio temporal attention' or 'factorised encoder' 
+    """
+
+    # (1, 1, d_model) -> model_custom cls token
+    # (1, 1, d_model) -> model_official cls token
+
+    if model_custom.model_name == 'spatio temporal attention' :
+        model_custom.encoder.cls.data[:] = model_official.cls_token.data
+    elif model_custom.model_name == 'factorised encoder':
+        model_custom.encoder.spacial_token.data[:] = model_official.cls_token.data
+
+
+
+def load_vivit_encoder_weights(model_custom, model_official):
+
+    """
+    Loads the encoder blocks' weights and biases from the pre-trained model to the current model, given a specific model_name/attention architecture.
+    These weights include normalisation layers, qkv layers, and mlp layers.
+
+    Parameters:
+        `model_custom`: The current ViViT model
+        `model_official`: The model which would be used to load the pre-trained weights
+        `model_name` (string): One of 'spatio temporal attention', 'factorised encoder', 'factorised self attention' or 'factorised dot product attention'
+    """
+
+    if model_custom.model_name == 'spatio temporal attention':
+
+        depth = len(model_custom.encoder.basicEncoder)
+
+        if model_custom.distilled:
+            for (name_custom, parameter_custom), (name_official, parameter_official) in zip(
+                list(model_custom.named_parameters())[6 : (12 * depth) + 6], 
+                list(model_official.named_parameters())[4 : 144 + 4]):
+
+                # print(f"{name_official} | {name_custom}")
+
+                parameter_custom.data[:] = parameter_official.data
+
+        else:
+            for (name_custom, parameter_custom), (name_official, parameter_official) in zip(
+                list(model_custom.named_parameters())[5 : (12 * depth) + 5], 
+                list(model_official.named_parameters())[4 : 144 + 4]):
+
+                # print(f"{name_official} | {name_custom}")
+
+                parameter_custom.data[:] = parameter_official.data
+
+    elif model_custom.model_name == 'factorised encoder':
+
+        spatial_depth = len(model_custom.encoder.spatialEncoder)
+        temporal_depth = len(model_custom.encoder.temporalEncoder)
+        
+        if model_custom.distilled:
+            #spatial
+            for (name_custom, parameter_custom), (name_official, parameter_official) in zip(
+                list(model_custom.named_parameters())[8 : (12 * spatial_depth) + 8], 
+                list(model_official.named_parameters())[4 : 144 + 4]):
+
+                # print(f"{name_official} | {name_custom}")
+
+                parameter_custom.data[:] = parameter_official.data
+
+            #temporal
+            for (name_custom, parameter_custom), (name_official, parameter_official) in zip(
+                list(model_custom.named_parameters())[144 + 8 : 144 + (12 * temporal_depth) + 8], 
+                list(model_official.named_parameters())[4 : 144 + 4]):
+
+                # print(f"{name_official} | {name_custom}")
+
+                parameter_custom.data[:] = parameter_official.data
+
+        else:
+            #spatial
+            for (name_custom, parameter_custom), (name_official, parameter_official) in zip(
+                list(model_custom.named_parameters())[6 : (12 * spatial_depth) + 6], 
+                list(model_official.named_parameters())[4 : 144 + 4]):
+
+                # print(f"{name_official} | {name_custom}")
+
+                parameter_custom.data[:] = parameter_official.data
+
+            #temporal
+            for (name_custom, parameter_custom), (name_official, parameter_official) in zip(
+                list(model_custom.named_parameters())[144 + 6 : 144 + (12 * temporal_depth) + 6], 
+                list(model_official.named_parameters())[4 : 144 + 4]):
+
+                # print(f"{name_official} | {name_custom}")
+
+                parameter_custom.data[:] = parameter_official.data
+    
+    elif model_custom.model_name == 'factorised self attention':
+
+        depth = len(model_custom.encoder.encoder)
+
+        for i in range(depth):
+            model_custom.encoder.encoder[i].layer_norm_1.weight.data[:] = model_official.blocks[i].norm1.weight.data
+            model_custom.encoder.encoder[i].layer_norm_1.bias.data[:] = model_official.blocks[i].norm1.bias.data
+
+            model_custom.encoder.encoder[i].spatial_attention.qkv.weight.data[:] = model_official.blocks[i].attn.qkv.weight.data
+            model_custom.encoder.encoder[i].spatial_attention.qkv.bias.data[:] = model_official.blocks[i].attn.qkv.bias.data
+
+            model_custom.encoder.encoder[i].spatial_attention.projection_layer.weight.data[:] = model_official.blocks[i].attn.proj.weight.data
+            model_custom.encoder.encoder[i].spatial_attention.projection_layer.bias.data[:] = model_official.blocks[i].attn.proj.bias.data
+
+            model_custom.encoder.encoder[i].layer_norm_2.weight.data[:] = model_official.blocks[i].norm2.weight.data
+            model_custom.encoder.encoder[i].layer_norm_2.bias.data[:] = model_official.blocks[i].norm2.bias.data
+
+            model_custom.encoder.encoder[i].mlp.fully_connected_1.weight.data[:] = model_official.blocks[i].mlp.fc1.weight.data
+            model_custom.encoder.encoder[i].mlp.fully_connected_1.bias.data[:] = model_official.blocks[i].mlp.fc1.bias.data
+
+            model_custom.encoder.encoder[i].mlp.fully_connected_2.weight.data[:] = model_official.blocks[i].mlp.fc2.weight.data
+            model_custom.encoder.encoder[i].mlp.fully_connected_2.bias.data[:] = model_official.blocks[i].mlp.fc2.bias.data
+
+    elif model_custom.model_name == 'factorised dot product attention':
+
+        depth = len(model_custom.encoder.encoder)
+
+        for i in range(depth):
+            model_custom.encoder.encoder[i].layer_norm_1.weight.data[:] = model_official.blocks[i].norm1.weight.data
+            model_custom.encoder.encoder[i].layer_norm_1.bias.data[:] = model_official.blocks[i].norm1.bias.data
+
+            model_custom.encoder.encoder[i].attention.qkv.weight.data[:] = model_official.blocks[i].attn.qkv.weight.data
+            model_custom.encoder.encoder[i].attention.qkv.bias.data[:] = model_official.blocks[i].attn.qkv.bias.data
+
+            model_custom.encoder.encoder[i].attention.projection_layer.weight.data[:] = model_official.blocks[i].attn.proj.weight.data
+            model_custom.encoder.encoder[i].attention.projection_layer.bias.data[:] = model_official.blocks[i].attn.proj.bias.data
+
+            model_custom.encoder.encoder[i].layer_norm_2.weight.data[:] = model_official.blocks[i].norm2.weight.data
+            model_custom.encoder.encoder[i].layer_norm_2.bias.data[:] = model_official.blocks[i].norm2.bias.data
+
+            model_custom.encoder.encoder[i].mlp.fully_connected_1.weight.data[:] = model_official.blocks[i].mlp.fc1.weight.data
+            model_custom.encoder.encoder[i].mlp.fully_connected_1.bias.data[:] = model_official.blocks[i].mlp.fc1.bias.data
+
+            model_custom.encoder.encoder[i].mlp.fully_connected_2.weight.data[:] = model_official.blocks[i].mlp.fc2.weight.data
+            model_custom.encoder.encoder[i].mlp.fully_connected_2.bias.data[:] = model_official.blocks[i].mlp.fc2.bias.data
+
+
+def load_classification_weights(model_custom, model_official):
+
+    """
+    Loads the classifier's weights and biases from the pre-trained model to the current model.
+
+    Parameters:
+        `model_custom`: The current model
+        `model_official`: The model which would be used to load the pre-trained weights
+    """
+
+    for (name_custom, parameter_custom), (name_official, parameter_official) in zip(
+        list(model_custom.named_parameters())[-4:], list(model_official.named_parameters())[-4:]):
+
+        # print(f"{name_official} | {name_custom}")
+
+        parameter_custom.data[:] = parameter_official.data
+
+
+def load_bimodal_encoder_weights(model_custom, model_official):
+
+    """
+    Loads the encoder blocks' weights and biases from the pre-trained model to the current model.
+    These weights include normalisation layers, qkv layers, and mlp layers.
+
+    Parameters:
+        `model_custom`: The current bi-modal model
+        `model_official`: The model which would be used to load the pre-trained weights
+    """
+
+    depth = len(model_custom.bi_modal_encoder)
+    d_model = model_custom.d_model
+
+    for i in range(depth):
+        # Layer Norm
+        model_custom.bi_modal_encoder[i].layer_norm_av_1.weight.data[:] = model_official.blocks[i].norm1.weight.data
+        model_custom.bi_modal_encoder[i].layer_norm_av_1.bias.data[:] = model_official.blocks[i].norm1.bias.data
+
+        model_custom.bi_modal_encoder[i].layer_norm_va_1.weight.data[:] = model_official.blocks[i].norm1.weight.data
+        model_custom.bi_modal_encoder[i].layer_norm_va_1.bias.data[:] = model_official.blocks[i].norm1.bias.data
+
+        model_custom.bi_modal_encoder[i].layer_norm_av_2.weight.data[:] = model_official.blocks[i].norm2.weight.data
+        model_custom.bi_modal_encoder[i].layer_norm_av_2.bias.data[:] = model_official.blocks[i].norm2.bias.data
+
+        model_custom.bi_modal_encoder[i].layer_norm_va_2.weight.data[:] = model_official.blocks[i].norm2.weight.data
+        model_custom.bi_modal_encoder[i].layer_norm_va_2.bias.data[:] = model_official.blocks[i].norm2.bias.data
+
+
+        # Cross Attention
+        model_custom.bi_modal_encoder[i].attention_av.q_linear.weight.data[:] = model_official.blocks[i].attn.qkv.weight.data[:d_model]
+        model_custom.bi_modal_encoder[i].attention_av.q_linear.bias.data[:] = model_official.blocks[i].attn.qkv.bias.data[:d_model]
+
+        model_custom.bi_modal_encoder[i].attention_av.k_linear.weight.data[:] = model_official.blocks[i].attn.qkv.weight.data[:d_model]
+        model_custom.bi_modal_encoder[i].attention_av.k_linear.bias.data[:] = model_official.blocks[i].attn.qkv.bias.data[:d_model]
+
+        model_custom.bi_modal_encoder[i].attention_av.v_linear.weight.data[:] = model_official.blocks[i].attn.qkv.weight.data[:d_model]
+        model_custom.bi_modal_encoder[i].attention_av.v_linear.bias.data[:] = model_official.blocks[i].attn.qkv.bias.data[:d_model]
+
+        model_custom.bi_modal_encoder[i].attention_va.q_linear.weight.data[:] = model_official.blocks[i].attn.qkv.weight.data[:d_model]
+        model_custom.bi_modal_encoder[i].attention_va.q_linear.bias.data[:] = model_official.blocks[i].attn.qkv.bias.data[:d_model]
+
+        model_custom.bi_modal_encoder[i].attention_va.k_linear.weight.data[:] = model_official.blocks[i].attn.qkv.weight.data[:d_model]
+        model_custom.bi_modal_encoder[i].attention_va.k_linear.bias.data[:] = model_official.blocks[i].attn.qkv.bias.data[:d_model]
+
+        model_custom.bi_modal_encoder[i].attention_va.v_linear.weight.data[:] = model_official.blocks[i].attn.qkv.weight.data[:d_model]
+        model_custom.bi_modal_encoder[i].attention_va.v_linear.bias.data[:] = model_official.blocks[i].attn.qkv.bias.data[:d_model]
+
+
+        # Projection layer
+        model_custom.bi_modal_encoder[i].attention_av.projection_layer.weight.data[:] = model_official.blocks[i].attn.proj.weight.data
+        model_custom.bi_modal_encoder[i].attention_av.projection_layer.bias.data[:] = model_official.blocks[i].attn.proj.bias.data
+
+        model_custom.bi_modal_encoder[i].attention_va.projection_layer.weight.data[:] = model_official.blocks[i].attn.proj.weight.data
+        model_custom.bi_modal_encoder[i].attention_va.projection_layer.bias.data[:] = model_official.blocks[i].attn.proj.bias.data
+
+        # MLP
+        model_custom.bi_modal_encoder[i].mlp_av.fully_connected_1.weight.data[:] = model_official.blocks[i].mlp.fc1.weight.data
+        model_custom.bi_modal_encoder[i].mlp_av.fully_connected_1.bias.data[:] = model_official.blocks[i].mlp.fc1.bias.data
+
+        model_custom.bi_modal_encoder[i].mlp_av.fully_connected_2.weight.data[:] = model_official.blocks[i].mlp.fc2.weight.data
+        model_custom.bi_modal_encoder[i].mlp_av.fully_connected_2.bias.data[:] = model_official.blocks[i].mlp.fc2.bias.data
+
+        model_custom.bi_modal_encoder[i].mlp_va.fully_connected_1.weight.data[:] = model_official.blocks[i].mlp.fc1.weight.data
+        model_custom.bi_modal_encoder[i].mlp_va.fully_connected_1.bias.data[:] = model_official.blocks[i].mlp.fc1.bias.data
+
+        model_custom.bi_modal_encoder[i].mlp_va.fully_connected_2.weight.data[:] = model_official.blocks[i].mlp.fc2.weight.data
+        model_custom.bi_modal_encoder[i].mlp_va.fully_connected_2.bias.data[:] = model_official.blocks[i].mlp.fc2.bias.data
