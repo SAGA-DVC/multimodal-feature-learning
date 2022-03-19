448053f098d655a07d89a932e2fefe30176737bb
unknown <acharekarganadhish1999@gmail.com>
2022-03-19 23:09:25 +0530

    added Deformable DETR model

---
+++ models/Deformable DETR/base_encoder.py
@@ -0,0 +1,88 @@
+"""
+Base Encoder to create multi-level conv features and positional embedding.
+"""
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+from modules import NestedTensor
+from modules import PositionEmbeddingSine
+
+
+class BaseEncoder(nn.Module):
+    '''Args:
+        num_feature_levels: number of feature levels in multiscale Deformable Attention (default=4)
+        vf_dim: dim of frame-level feature vector (default = 500)
+        hidden_dim: Dimensionality of the hidden layer in the feed-forward networks within the Transformer (default=512).
+    '''
+    def __init__(self, num_feature_levels, vf_dim, hidden_dim):
+        super(BaseEncoder, self).__init__()
+        self.pos_embed = PositionEmbeddingSine(hidden_dim//2, normalize=True)
+        self.num_feature_levels = num_feature_levels
+        self.hidden_dim = hidden_dim
+
+        #   creating base_encoder
+        if num_feature_levels > 1:
+            input_proj_list = []
+            in_channels = vf_dim
+            input_proj_list.append(nn.Sequential(
+                nn.Conv1d(in_channels, hidden_dim, kernel_size=1),
+                nn.GroupNorm(32, hidden_dim),
+            ))
+            for _ in range(num_feature_levels - 1):
+                input_proj_list.append(nn.Sequential(
+                    nn.Conv1d(in_channels, hidden_dim, kernel_size=3, stride=2, padding=1),
+                    nn.GroupNorm(32, hidden_dim),
+                ))
+                in_channels = hidden_dim
+            self.input_proj = nn.ModuleList(input_proj_list)
+        else:
+            self.input_proj = nn.ModuleList([
+                nn.Sequential(
+                    nn.Conv2d(vf_dim, hidden_dim, kernel_size=1),
+                    nn.GroupNorm(32, hidden_dim),
+                )])
+
+        for proj in self.input_proj:
+            nn.init.xavier_uniform_(proj[0].weight, gain=1)
+            nn.init.constant_(proj[0].bias, 0)
+
+    def forward(self, vf, mask, duration):
+        """
+        :param vf: video tensor, expected shape: (batch_size, num_token, dmodel)
+        :param mask: video mask, expected shape: (batch_size, num_token)
+        :param duration: video length, expected shape: (batch_size)
+        :return: srcs list[[batch_size, dmodel, num-token]]
+        :return: masks list[[batch_size, num-token]]
+        :return: poses list[[batch_size, dmodel, num-token]]
+        """
+        vf = vf.transpose(1, 2)  # (batch_size, num_token, dmodel) --> (batch_size, dmodel, num_token)
+        vf_nt = NestedTensor(vf, mask, duration)
+        pos0 = self.pos_embed(vf_nt) 
+
+        srcs = []
+        masks = []
+        poses = []
+
+        src0, mask0 = vf_nt.decompose() #   return vf,mask
+        srcs.append(self.input_proj[0](src0))
+        masks.append(mask0)
+        poses.append(pos0)
+        assert mask is not None
+
+        for l in range(1, self.num_feature_levels):
+            if l == 1:
+                src = self.input_proj[l](vf_nt.tensors)
+            else:
+                src = self.input_proj[l](srcs[-1])
+            m = vf_nt.mask
+            mask = F.interpolate(m[None].float(), size=src.shape[-1:]).to(torch.bool)[0] #  upsample the mask to given shape
+            pos_l = self.pos_embed(NestedTensor(src, mask, duration)).to(src.dtype)
+            srcs.append(src)
+            masks.append(mask)
+            poses.append(pos_l)
+        return srcs, masks, poses
+
+def build_base_encoder(args):
+    base_encoder = BaseEncoder(args.num_feature_levels, args.feature_dim, args.hidden_dim)
+    return base_encoder
---
+++ models/Deformable DETR/run_deformable_transformer.py
@@ -0,0 +1,64 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+from deformable_transformer import build_deforamble_transformer
+from base_encoder import build_base_encoder
+from config import load_config
+from modules import decide_two_stage
+
+
+cfg = load_config()
+base_encoder = build_base_encoder(cfg.detr)
+transformer = build_deforamble_transformer(cfg.detr)
+
+
+# vf = dt['video_tensor']  # (batch_size, num_token, dmodel) or  (video_num, video_len, video_dim)
+# mask = ~ dt['video_mask']  # (batch_size, num_token)
+# duration = dt['video_length'][:, 1] # (batch_size)
+vf = torch.rand([2, 10, 500])
+mask = torch.randint(1,(2, 10))
+duration = torch.rand([2])
+
+# criterion = SetCriterion(args.num_classes, matcher, weight_dict, losses, focal_alpha=args.focal_alpha,,focal_gamma=args.focal_gamma, opt=args)
+criterion = {}
+
+N, L, C = vf.shape
+# assert N == 1, "batch size must be 1."
+query_embed = nn.Embedding(cfg.detr.num_queries, cfg.detr.hidden_dim * 2)
+
+
+#   base encoder
+srcs, masks, pos = base_encoder(vf, mask, duration)
+#   print("src shape: ", len(srcs))    #   [[2, 512, 10]  [2, 512, 5] [2, 512, 3] [2, 512, 2]]   list[[batch_size, dmodel, num-token]]
+#   print("masks shape: ", len(masks))    #   [[2,10], [2,5], [2,3], [2,2]]
+#   print("pos shape: ", len(pos))    #   [[2, 512, 10]  [2, 512, 5] [2, 512, 3] [2, 512, 2]]
+
+
+#   forword encoder
+src_flatten, temporal_shapes, level_start_index, valid_ratios, lvl_pos_embed_flatten, mask_flatten = transformer.prepare_encoder_inputs(srcs, masks, pos)
+memory = transformer.forward_encoder(src_flatten, temporal_shapes, level_start_index, valid_ratios, lvl_pos_embed_flatten, mask_flatten)    #   (batch_size, sum of num_token in all level, dmodel) #   Multi-scale frame features
+
+
+#   decoder
+transformer_input_type = "queries"
+gt_boxes = torch.rand([2, 3, 2])
+gt_boxes_mask = torch.randint(1,(2, 3))
+
+two_stage, disable_iterative_refine, proposals, proposals_mask = decide_two_stage(transformer_input_type,
+                                                                                        gt_boxes, gt_boxes_mask, criterion)
+
+if two_stage:
+    init_reference, tgt, reference_points, query_embed = transformer.prepare_decoder_input_proposal(proposals)
+else:
+    query_embed = query_embed.weight
+    proposals_mask = torch.ones(N, query_embed.shape[0], device=query_embed.device).bool()  #   (batch_size, num_queries)
+    init_reference, tgt, reference_points, query_embed = transformer.prepare_decoder_input_query(memory, query_embed)
+
+output, inter_references = transformer.forward_decoder(tgt, reference_points, memory, temporal_shapes,
+                                                        level_start_index, valid_ratios, query_embed,
+                                                        mask_flatten, proposals_mask, disable_iterative_refine)
+
+
+#   output -> (number of decoder_layers, batch_size, num_queries, dmodel)
+#   inter_reference = (number of decoder_layers, batch_size, num_queries, 1)
+
