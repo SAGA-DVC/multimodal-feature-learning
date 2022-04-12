9a249e5f8995358aa681656e25c1f513ac3f7dc2
Akshat Shah <akshatshah_21@yahoo.in>
2022-04-13 01:03:51 +0530

    Update epoch loop to show plots with print_freq

--- tsp/engine.py
+++ tsp/engine.py
@@ -51,13 +51,10 @@ def epoch_loop(model: TSPModel, criterion, optimizer, dataloader, device, epoch,
         optimizer.zero_grad()
         loss.backward()
 
-        if batch_idx % 50 == 0:
-            plots.plot_grad_flow_line(model.named_parameters(
-            ), epoch=epoch, batch_idx=batch_idx, prefix='fc', output_dir=output_dir)
-            plots.plot_grad_flow_line(model.backbones[0].named_parameters(
-            ), epoch=epoch, batch_idx=batch_idx, prefix='vivit', output_dir=output_dir)
-            plots.plot_grad_flow_line(model.backbones[1].named_parameters(
-            ), epoch=epoch, batch_idx=batch_idx, prefix='ast', output_dir=output_dir)
+        if utils.is_main_process() and batch_idx % print_freq == 0:
+            plots.plot_grad_flow_line(model.module..named_parameters(), epoch=epoch, batch_idx=batch_idx, prefix='fc', output_dir=output_dir)
+            plots.plot_grad_flow_line(model.module.backbones[0].named_parameters(), epoch=epoch, batch_idx=batch_idx, prefix='vivit', output_dir=output_dir)
+            plots.plot_grad_flow_line(model.module.backbones[1].named_parameters(), epoch=epoch, batch_idx=batch_idx, prefix='ast', output_dir=output_dir)
 
         optimizer.step()
 
