Grad Flow and corrected non-differentiable context module - [Add] Visualize gradient flow [Fix] Use start and end time instead of center and length for context features [Add] Context features depending on scale in unimodal_deformable_detr [Fix] Commented pos_trans in unimodal_deformable_transformer (else grad is None)

engine.py
@@ -14,6 +14,65 @@ from utils.misc import MetricLogger, SmoothedValue, reduce_dict
 from utils.preds_postprocess import get_sample_submission, get_src_permutation_idx, denormalize_segments, captions_to_string, pprint_eval_scores, save_submission
 from evaluation.evaluate import run_eval
 
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
+
+def plot_grad_flow_v1(named_parameters, epoch, batch_idx):
+    ave_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if p.grad == None:
+            print(n, p.requires_grad)
+            print("Grad None!!")
+        if(p.requires_grad) and ("bias" not in n) and (p.grad != None):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean().cpu())
+    plt.plot(ave_grads, alpha=0.3, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical", fontsize=5)
+    plt.xlim(xmin=0, xmax=len(ave_grads))
+    plt.ylim(bottom = -0.001, top=0.005) # zoom in on the lower gradient regions
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.savefig('output/grads/E{}_B{}_line.png'.format(epoch, batch_idx), bbox_inches='tight')
+
+
+def plot_grad_flow_v2(named_parameters, epoch, batch_idx):
+    '''Plots the gradients flowing through different layers in the net during training.
+    Can be used for checking for possible gradient vanishing / exploding problems.
+    
+    Usage: Plug this function in Trainer class after loss.backwards() as 
+    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow'''
+    ave_grads = []
+    max_grads= []
+    layers = []
+    for n, p in named_parameters:
+        if p.grad == None:
+            print(n, p.requires_grad)
+            print("Grad None!!")
+        if(p.requires_grad) and ("bias" not in n) and (p.grad != None):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean().cpu())
+            max_grads.append(p.grad.abs().max().cpu())
+    plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+    plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, lw=2, color="k" )
+    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical", fontsize=5)
+    plt.xlim(left=0, right=len(ave_grads))
+    plt.ylim(bottom = -0.001, top=0.005) # zoom in on the lower gradient regions
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.legend([Line2D([0], [0], color="c", lw=4),
+                Line2D([0], [0], color="b", lw=4),
+                Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])
+    plt.savefig('output/grads/E{}_B{}_bar.png'.format(epoch, batch_idx), bbox_inches='tight')
+
 
 def train_one_epoch(model, criterion, data_loader, optimizer, device, epoch, args, wandb_log, wandb):
     
@@ -41,7 +100,7 @@ def train_one_epoch(model, criterion, data_loader, optimizer, device, epoch, arg
     header = f'Epoch: [{epoch}]'
     print_freq = 1
 
-    for obj in metric_logger.log_every(data_loader, print_freq, wandb_log, wandb, header):
+    for (batch_idx, obj) in enumerate(metric_logger.log_every(data_loader, print_freq, wandb_log, wandb, header)):
 
         obj = {key: v.to(device) if isinstance(v, torch.Tensor) else v for key, v in obj.items()}
         obj['video_target'] = [{key: v.to(device) if isinstance(v, torch.Tensor) else v for key, v in vid_info.items()} 
@@ -72,6 +131,10 @@ def train_one_epoch(model, criterion, data_loader, optimizer, device, epoch, arg
 
         optimizer.zero_grad()
         losses.backward()
+
+        # plot_grad_flow_v1(model.named_parameters(), epoch, batch_idx)
+        # plot_grad_flow_v2(model.named_parameters())
+
         if args.clip_max_norm > 0:
             clip_grad_norm_(model.parameters(), args.clip_max_norm)
         optimizer.step()

models/unimodal_deformable_dvc.py
@@ -2,6 +2,8 @@
 DVC model for event segmentation and captioning
 """
 
+from math import floor
+
 import torch
 import torch.nn as nn
 from torch.nn.init import trunc_normal_
@@ -18,6 +20,7 @@ from .modules.layers import FFN
 
 from .load_weights import load_positional_embeddings
 
+from utils.preds_postprocess import get_src_permutation_idx, denormalize_segments
 
 # TODO - src mask
 # TODO - check devices for tensors
@@ -71,6 +74,9 @@ class UnimodalDeformableDVC(nn.Module):
         # Unimodal Deformable DETR
         self.unimodal_deformable_transformer = build_unimodal_deformable_transformer(detr_args)
         
+        # Context Module
+        self.num_feature_levels = detr_args.num_feature_levels
+
         # Captioning module
         self.caption_decoder = build_caption_decoder(caption_args, vocab_size, seq_len, embedding_matrix)
         
@@ -181,14 +187,13 @@ class UnimodalDeformableDVC(nn.Module):
         indices = self.matcher(out, obj['video_target']) 
 
         # Context Features
-        # with torch.no_grad():
-        max_gt_target_segments = obj['gt_segments'].shape[1]
+        video_durations = list(obj['video_length'][:, 1])
 
         # (nb_target_segments, num_tokens, d_model), (nb_target_segments, num_tokens)
-        memory, memory_mask = self.get_segment_features(video, out['pred_segments'], indices, max_gt_target_segments)
+        memory, memory_mask = self.get_segment_features(memory, out['pred_segments'], indices, video_durations)
+        # print(memory.shape, (memory_mask==1).sum())
 
         memory = memory.to(video.device)
-        memory.requires_grad = True
 
         memory_mask = memory_mask.unsqueeze(1).unsqueeze(1)    # (nb_target_segments, 1, 1, num_tokens)
         memory_mask = memory_mask.to(video.device)
@@ -314,9 +319,7 @@ class UnimodalDeformableDVC(nn.Module):
         return tgt_padding_mask
 
 
-    # TODO - make more efficient
-    def get_segment_features(self, features, pred_segments, indices, max_gt_target_segments):
-
+    def get_segment_features(self, features, pred_segments, indices, video_durations):
         """
         Gets features within a specific boundary (based on selected bipartite matching indices) from pre-computed video features
         Parameters:
@@ -324,87 +327,58 @@ class UnimodalDeformableDVC(nn.Module):
             pred_segments : Tensor of dimension (batch_size, num_queries, 2). These are the pre-computed event/segment boundaries.
             indices : matching between the outputs of the last layer and the targets
                     list (len=batch_size) of tuple of tensors (shape=(2, gt_target_segments))
-            max_gt_target_segments (int): Maximum number of ground truth events/segments in a single video in a batch
+            video_durations (tensor, float): (batch_size,), representing duration of videos
 
         Returns:
             pred_features : Tensor of dimension (nb_target_segments, num_tokens, d_model)
             pred_features_src_padding_mask : Tensor of dimension (nb_target_segments, num_tokens)
         """
         
-        batch_size, num_tokens, d_model = features.shape
+        idx = get_src_permutation_idx(indices)
+        denormalized_segments = denormalize_segments(pred_segments[idx], video_durations, idx[0])
 
-        pred_segment_boundaries = torch.zeros(batch_size, max_gt_target_segments, 2)
+        pred_features, pred_features_src_padding_mask = self.crop_segments(features, denormalized_segments, idx[0], video_durations)
 
-        for i, (pred_idx, _) in enumerate(indices):
-            pred_segment_boundaries[i, :pred_idx.shape[0]] = pred_segments[i, pred_idx.long()]
-        
-        # (batch_size, max_gt_target_segments, num_tokens, d_model) AND (batch_size, max_gt_target_segments, num_tokens) AND (batch_size, max_gt_target_segments)
-        pred_features, pred_features_src_padding_mask, pred_segments_padding_mask = self.crop_segments(features, pred_segment_boundaries, indices, max_gt_target_segments)
-        
-        pred_features = pred_features.reshape(-1, num_tokens, d_model)
-        pred_features_src_padding_mask = pred_features_src_padding_mask.reshape(-1, num_tokens)
-        pred_segments_padding_mask = pred_segments_padding_mask.reshape(-1)
-        
-        # removes extra captions (padding) added to satisfy dimension constraints of tensors
-        pred_features = pred_features[pred_segments_padding_mask == True]    # (nb_target_segments, num_tokens, d_model)
-        pred_features_src_padding_mask = pred_features_src_padding_mask[pred_segments_padding_mask == True]    # (nb_target_segments, num_tokens)
-        
         return pred_features, pred_features_src_padding_mask
 
 
-    # TODO - padding like in BMT??
-    # TODO - start end time to center length
-    def crop_segments(self, features, pred_segment_boundaries, indices, max_gt_target_segments):
-
+    def crop_segments(self, features, denormalized_segments, segment_batch_id, video_durations):
         """
         Crops the video features within a specific boundary (based on selected bipartite matching indices)
         Parameters:
             features : Tensor of dimension (batch_size, num_tokens, d_model). These are the pre-computed features
-            pred_segment_boundaries : Tensor of dimension (batch_size, max_gt_target_segments, 2). These are the pre-computed event/segment boundaries.
-            indices : matching between the outputs of the last layer and the targets
-                    list (len=batch_size) of tuple of tensors (shape=(2, gt_target_segments))
-            max_gt_target_segments (int): Maximum number of ground truth events/segments in a single video in a batch
+            denormalized_segments : Tensor of dimension (nb_target_segments, 2). start time and end time of selected segments
+            segment_batch_id (tensor, int): (num_proposals,), representing batch id of corresponding segment
+            video_durations (tensor, float): (batch_size,), representing duration of videos
 
         Returns:
             pred_features : Tensor of dimension (batch_size, max_gt_target_segments, num_tokens, d_model)
-            pred_features_src_padding_mask : Tensor of dimension (batch_size, max_gt_target_segments, num_tokens)
-            pred_segments_padding_mask : Tensor of dimension (batch_size, max_gt_target_segments)
-            
+            pred_features_src_padding_mask : Tensor of dimension (batch_size, max_gt_target_segments, num_tokens)            
         """
 
         batch_size, num_tokens, d_model = features.shape
-        
-        start_quantile = pred_segment_boundaries[:, :, 0]    # (batch_size, max_gt_target_segments)
-        end_quantile = pred_segment_boundaries[:, :, 1]    # (batch_size, max_gt_target_segments)
-        start_idx = (num_tokens * start_quantile).long().reshape(-1)    # (batch_size * max_gt_target_segments)
-        end_idx = (num_tokens * end_quantile).long().reshape(-1)    # (batch_size * max_gt_target_segments)
 
-        for i, (start, end) in enumerate(zip(start_idx, end_idx)):
-            if start >= end:
-                if start >= num_tokens:
-                    start = num_tokens - 1
-                    start_idx[i] = start
-                    end_idx[i] = num_tokens
-                elif end >= num_tokens:
-                    end_idx[i] = num_tokens
-                else:
-                    end_idx[i] = start + 1
-        
-        start_idx = start_idx.reshape(batch_size, max_gt_target_segments) 
-        end_idx = end_idx.reshape(batch_size, max_gt_target_segments)
-            
-        pred_features = torch.zeros(batch_size, max_gt_target_segments, num_tokens, d_model)
-        pred_features_src_padding_mask = torch.zeros(batch_size, max_gt_target_segments, num_tokens)
-        pred_segments_padding_mask = torch.zeros(batch_size, max_gt_target_segments, dtype=torch.bool)
+        # normalize segments with respect to duration
+        durations_per_proposal = torch.tensor([video_durations[batch_id] for batch_id in segment_batch_id])
 
-        for i in range(batch_size):
-            gt_target_segments = len(indices[i][0])
-            for j in range(gt_target_segments):
-                pred_features[i, j, start_idx[i, j]:end_idx[i, j]] = features[i, start_idx[i, j]:end_idx[i, j], :]
-                pred_features_src_padding_mask[i, j, start_idx[i, j]:end_idx[i, j]] = 1
-                pred_segments_padding_mask[i, j] = True
+        pred_features = torch.zeros([denormalized_segments.shape[0], num_tokens, d_model])
+        pred_features_src_padding_mask = torch.zeros([denormalized_segments.shape[0], num_tokens])
 
-        return pred_features, pred_features_src_padding_mask, pred_segments_padding_mask 
+        video_rescale_len = floor((2**(self.num_feature_levels - 1) / (2**self.num_feature_levels - 1)) * num_tokens)
+
+        for n in range(self.num_feature_levels):
+            lower_limit = floor(video_rescale_len * ((2**n - 1) / 2**(n - 1)))
+            upper_limit = floor(video_rescale_len * ((2**(n + 1) - 1) / 2**n))
+            diff = upper_limit - lower_limit
+
+            start_token = torch.clamp((lower_limit + (diff * denormalized_segments[:, 0] / durations_per_proposal)).round().long(), min=lower_limit, max=upper_limit-1)
+            end_token = torch.clamp((lower_limit + (diff * denormalized_segments[:, 1] / durations_per_proposal)).round().long(), min=lower_limit, max=upper_limit-1)
+
+            for i, batch_id in enumerate(segment_batch_id):
+                pred_features[i, start_token[i]:end_token[i]] = features[batch_id, start_token[i]:end_token[i], :]
+                pred_features_src_padding_mask[i, start_token[i]:end_token[i]] = 1
+
+        return pred_features, pred_features_src_padding_mask
 
     
     def init_weights(self):