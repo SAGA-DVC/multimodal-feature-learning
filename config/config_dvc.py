e2c79e8c06726e4fd3c9de58cf8c876c0fead273
unknown <acharekarganadhish1999@gmail.com>
2022-04-23 18:44:10 +0530

    [Add] sparse DETR model

--- config/config_dvc.py
+++ config/config_dvc.py
@@ -168,6 +168,29 @@ def load_config():
     cfg.dvc.detr.return_intermediate = True    # TODO - check use
     
 
+    # Sparse DETR
+    cfg.dvc.sparse_detr = ml_collections.ConfigDict()
+    cfg.dvc.sparse_detr.type = "audio_video"
+    cfg.dvc.sparse_detr.feature_dim = 768  #   dim of frame-level feature vector (default = 500)
+    cfg.dvc.sparse_detr.d_model= 768
+    cfg.dvc.sparse_detr.hidden_dim = 768   #   Dimensionality of the hidden layer in the feed-forward networks within the Transformer
+    cfg.dvc.sparse_detr.num_queries = 100  #   number of queries givin to decoder
+    cfg.dvc.sparse_detr.hidden_dropout_prob = 0.5
+    cfg.dvc.sparse_detr.layer_norm_eps = 1e-12 
+    cfg.dvc.sparse_detr.num_heads = 8 #   the number of heads in the multiheadattention models
+    cfg.dvc.sparse_detr.num_feature_levels = 4  #  number of feature levels in multiscale Deformable Attention 
+    cfg.dvc.sparse_detr.dec_n_points = 4   #   number of sampling points per attention head per feature level for decoder
+    cfg.dvc.sparse_detr.enc_n_points = 4   #   number of sampling points per attention head per feature level for encoder
+    cfg.dvc.sparse_detr.enc_layers = 6 #   number of sub-encoder-layers in the encoder
+    cfg.dvc.sparse_detr.dec_layers = 6 #   number of sub-decoder-layers in the decode
+    cfg.dvc.sparse_detr.transformer_dropout_prob = 0.1 #   the dropout value
+    cfg.dvc.sparse_detr.transformer_ff_dim = 2048  #    the dimension of the feedforward network model
+    cfg.dvc.sparse_detr.rho=0.3 
+    cfg.dvc.sparse_detr.use_enc_aux_loss=False
+    cfg.dvc.sparse_detr.eff_query_init=False
+    cfg.dvc.sparse_detr.eff_specific_head=False
+    cfg.dvc.sparse_detr.return_intermediate=False
+
     # Caption Decoder
     # vocab_size, seq_len, embedding_matrix - these parameters are set in /models/__init__.py
     cfg.dvc.caption = ml_collections.ConfigDict()
